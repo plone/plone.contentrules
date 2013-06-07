43b769d48244bc9c48b4e5ddc01456672367f203
tdesvenain <thomas.desvenain@gmail.com>
2013-06-07 12:13:43 +0200

    Migrate on-the-fly added event dotted name +    if Plone has been migrated from any release to 4.3 release. +    Avoids any upgrade to fail when setup profile is re-imported.

--- plone/contentrules/engine/assignments.py
+++ plone/contentrules/engine/assignments.py
@@ -1,11 +1,13 @@
 from persistent import Persistent
 
+from OFS.Uninstalled import BrokenClass
+from ZODB.broken import PersistentBroken
 from zope.interface import implements, implementer
 from zope.component import adapter, queryUtility
-
 from zope.annotation.interfaces import IAnnotations
 from zope.container.ordered import OrderedContainer
 from zope.container.contained import Contained
+from zope.container.interfaces import IObjectAddedEvent
 
 from plone.contentrules.engine.interfaces import IRuleStorage
 from plone.contentrules.engine.interfaces import IRuleAssignable
@@ -13,6 +15,16 @@ from plone.contentrules.engine.interfaces import IRuleAssignment
 from plone.contentrules.engine.interfaces import IRuleAssignmentManager
 
 from BTrees.OOBTree import OOBTree
+
+
+def check_rules_with_dotted_name_moved(rule):
+    """Migrate on-the-fly added event dotted name
+    if Plone has been migrated from any release to 4.3 release.
+    Avoids any upgrade to fail when setup profile is re-imported.
+    """
+    if PersistentBroken in rule.event.__bases__ or BrokenClass in rule.event.__bases__:
+        if rule.event.__name__ == 'IObjectAddedEvent':
+            rule.event = IObjectAddedEvent
 
 KEY = 'plone.contentrules.localassignments'
 
@@ -43,10 +55,24 @@ class RuleAssignmentManager(OrderedContainer):
         storage = queryUtility(IRuleStorage)
         if storage is not None:
             for a in self.values():
-                if a.enabled and (bubbled == False or a.bubbles):
-                    r = storage.get(a.__name__, None)
-                    if r is not None and r.enabled and r.event.providedBy(event):
-                        rules.append(r)
+                if not a.enabled:
+                    continue
+                if not (bubbled == False or a.bubbles):
+                    continue
+
+                r = storage.get(a.__name__, None)
+                if r is None:
+                    continue
+
+                try:
+                    provided = r.event.providedBy(event)
+                except AttributeError:
+                    check_rules_with_dotted_name_moved(r)
+                    provided = r.event.providedBy(event)
+
+                if provided and r.enabled:
+                    rules.append(r)
+
         return rules
 
 @adapter(IRuleAssignable)
@@ -60,4 +86,5 @@ def ruleAssignmentManagerAdapterFactory(context):
     manager = annotations.get(KEY, None)
     if manager is None:
         manager = annotations[KEY] = RuleAssignmentManager()
+
     return manager
