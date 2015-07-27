49545c1ee186e125adda76052633e35ba60df40c
Gil Forcada Codinachs <gforcada@gnome.org>
2015-07-27 14:48:25 +0200

    txt -> rst

--- setup.py
+++ setup.py
@@ -5,8 +5,8 @@ version = '2.0.5.dev0'
 setup(name='plone.contentrules',
       version=version,
       description="Plone ContentRules Engine",
-      long_description=open("README.txt").read() + "\n" +
-                       open("CHANGES.txt").read(),
+      long_description=open("README.rst").read() + "\n" +
+                       open("CHANGES.rst").read(),
       classifiers=[
           "Environment :: Web Environment",
           "Framework :: Plone",
