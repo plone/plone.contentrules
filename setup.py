6537bf317ef423e67707cf231b5ff97fefdfac6c
Maurits van Rees <maurits@vanrees.org>
2018-10-01 15:38:15 +0200

    Fixed PyPI links in setup.py.
    [ci skip]

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ setup(
     keywords='Plone content rules events',
     author='Plone Foundation',
     author_email='plone-developers@lists.sourceforge.net',
-    url='https://pypi.python.org/pypi/plone.contentrules',
+    url='https://pypi.org/project/plone.contentrules',
     license='GPL version 2',
     packages=find_packages(exclude=['ez_setup']),
     namespace_packages=['plone'],
