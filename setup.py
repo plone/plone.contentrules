0ff8a4cfc98a2b237d8138c0877b1fd3acb9b403
Gil Forcada Codinachs <gil.gnome@gmail.com>
2023-03-13 01:24:15 +0100

    Drop the 3 on ZODB3 dependency

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ setup(
     install_requires=[
       'setuptools',
       'six',
-      'ZODB3',
+      'ZODB',
       'zope.annotation',
       'zope.component',
       'zope.componentvocabulary',
