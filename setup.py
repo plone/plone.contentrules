c759e0a9c0d6daff7c2b0c2d1fe83369fccfaeb6
Gil Forcada Codinachs <gil.gnome@gmail.com>
2026-08-15 13:06:39 +0200

    feat: move metadata from setup.py to pyproject.toml.

--- setup.py
+++ setup.py
@@ -1,46 +1,4 @@
-from pathlib import Path
 from setuptools import setup
 
-version = "4.0.1.dev0"
-
-long_description = (
-    f"{Path('README.rst').read_text()}\n{Path('CHANGES.rst').read_text()}"
-)
-
-setup(
-    name="plone.contentrules",
-    version=version,
-    description="Plone ContentRules Engine",
-    long_description=long_description,
-    long_description_content_type="text/x-rst",
-    # Get more strings from
-    # https://pypi.org/classifiers/
-    classifiers=[
-        "Development Status :: 5 - Production/Stable",
-        "Environment :: Web Environment",
-        "Framework :: Plone",
-        "Framework :: Plone :: 6.2",
-        "Framework :: Plone :: Core",
-        "Framework :: Zope :: 5",
-        "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
-        "Operating System :: OS Independent",
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 3.10",
-        "Programming Language :: Python :: 3.11",
-        "Programming Language :: Python :: 3.12",
-        "Programming Language :: Python :: 3.13",
-        "Programming Language :: Python :: 3.14",
-    ],
-    keywords="Plone content rules events",
-    author="Plone Foundation",
-    author_email="plone-developers@lists.sourceforge.net",
-    url="https://pypi.org/project/plone.contentrules",
-    license="GPL version 2",
-    include_package_data=True,
-    zip_safe=False,
-    python_requires=">=3.10",
-    install_requires=[
-        "zope.componentvocabulary",
-        "Zope",
-    ],
-)
+# See pyproject.toml for package metadata
+setup()
